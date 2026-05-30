109b1a88e3863c8d349115ea223f302b14016ef2
diaszin <84644190+diaszin@users.noreply.github.com>
2026-05-30 19:57:23 -0300

    Remove home view

--- lib/router.dart
+++ lib/router.dart
@@ -1,10 +1,10 @@
-import 'package:desafio_tecnico_wtf/ui/movie/views/home_view.dart';
+import 'package:desafio_tecnico_wtf/ui/movie/views/all_movies_view.dart';
 import 'package:desafio_tecnico_wtf/ui/movie/views/login_view.dart';
 import 'package:go_router/go_router.dart';
 
 final _routes = [
   GoRoute(path: "/", builder: (context, state) => LoginView()),
-  GoRoute(path: "/home", builder: (context, state) => MyHomeView()),
+  GoRoute(path: "/home", builder: (context, state) => AllMoviesView()),
 ];
 
 final router = GoRouter(routes: _routes);
