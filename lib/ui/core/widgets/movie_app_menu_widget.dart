648112869ea0ee03692c516059619bd7ad8c5dd3
diaszin <84644190+diaszin@users.noreply.github.com>
2026-05-31 17:57:46 -0300

    Add link para página inicial quando clicar na logo

--- lib/ui/core/widgets/movie_app_menu_widget.dart
+++ lib/ui/core/widgets/movie_app_menu_widget.dart
@@ -1,6 +1,7 @@
 import 'package:desafio_tecnico_wtf/ui/core/widgets/app_title.dart';
 import 'package:desafio_tecnico_wtf/ui/core/widgets/search_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class MovieAppMenu extends StatelessWidget implements PreferredSizeWidget {
   const MovieAppMenu({super.key});
@@ -9,7 +10,12 @@ class MovieAppMenu extends StatelessWidget implements PreferredSizeWidget {
   AppBar build(BuildContext context) {
     return AppBar(
       backgroundColor: Color(0xFF1C1D21),
-      title: AppTitle(fontSize: 20, letterSpacing: -0.9,),
+      title: GestureDetector(
+        onTap: () {
+          context.go("/home");
+        },
+        child: AppTitle(fontSize: 20, letterSpacing: -0.9),
+      ),
       titleSpacing: 22,
       actions: [SearchButton()],
       actionsPadding: .only(right: 22),
