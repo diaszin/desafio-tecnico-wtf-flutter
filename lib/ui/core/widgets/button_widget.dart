fbf2b1acb619ee5dc8773d08d61193358249d70e
diaszin <84644190+diaszin@users.noreply.github.com>
2026-05-31 00:50:18 -0300

    Add atributos de largura e icone para botão

--- lib/ui/core/widgets/button_widget.dart
+++ lib/ui/core/widgets/button_widget.dart
@@ -1,31 +1,57 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
+enum ButtonType { primary, secondary }
+
 class Button extends StatelessWidget {
-  final String text;
+  final String? text;
   final VoidCallback onPressed;
+  final IconData? icon;
+  final double? width;
+  final ButtonType type;
+  final Color? iconColor;
 
   static void _defaultPressed() {}
 
   const Button({
     super.key,
-    required this.text,
+    this.text,
     this.onPressed = _defaultPressed,
+    this.icon,
+    this.width,
+    this.type = ButtonType.primary,
+    this.iconColor,
   });
 
   @override
   Widget build(BuildContext context) {
-    Size platform = MediaQuery.of(context).size;
-
-    return ElevatedButton(
-      onPressed: onPressed,
-      style: ElevatedButton.styleFrom(
-        backgroundColor: Color(0xFFEF233C),
-        fixedSize: Size(platform.width * 0.9, 57),
-        shape: RoundedRectangleBorder(borderRadius: .circular(16)),
+    Color buttonColor = switch (type) {
+      ButtonType.primary => Color(0xFFEF233C),
+      ButtonType.secondary => Color(0xFF1C1D21),
+    };
+    final buttonStyle = ElevatedButton.styleFrom(
+      backgroundColor: buttonColor,
+      fixedSize: Size(width ?? double.infinity, 57),
+      shape: RoundedRectangleBorder(
+        side: BorderSide(color: Color(0x14EDE8DD), width: 0.74),
+        borderRadius: .circular(16),
       ),
-      child: Text(
-        text,
+    );
+
+    if (text == null && icon != null) {
+      return IconButton(
+        onPressed: onPressed,
+        icon: Icon(icon, size: 15, color: iconColor),
+        style: buttonStyle,
+      );
+    }
+
+    return ElevatedButton.icon(
+      icon: Icon(icon, size: 15, color: iconColor),
+      onPressed: onPressed,
+      style: buttonStyle,
+      label: Text(
+        text!,
         style: GoogleFonts.roboto(
           fontWeight: .w600,
           fontSize: 14,
