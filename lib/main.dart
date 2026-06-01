c675b4eb4207378d44c3fa3f8c893693d02c0ed1
diaszin <84644190+diaszin@users.noreply.github.com>
2026-06-01 03:05:21 -0300

    Cria splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ import 'package:desafio_tecnico_wtf/ui/movie/view_models/movie_view_model.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:logger/logger.dart';
 import 'package:provider/provider.dart';
 
@@ -14,7 +15,9 @@ import 'domain/repository/movie_repository.dart';
 import 'ui/core/theme/app_theme.dart';
 
 Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
+  WidgetsBinding binding = WidgetsFlutterBinding.ensureInitialized();
+
+  FlutterNativeSplash.preserve(widgetsBinding: binding);
 
   Logger logger = Logger(
     filter: null,
@@ -74,6 +77,7 @@ Future<void> main() async {
       child: const MyApp(),
     ),
   );
+  FlutterNativeSplash.remove();
 }
 
 class MyApp extends StatelessWidget {
